f8160bda587d480133f3b14b13794cffe6ddf6ef
James Kelly <jk377y@gmail.com>
2023-05-31 03:24:18 -0500

    created outline for code

--- contacts.py
+++ contacts.py
@@ -1,1 +1,20 @@
-# test
+import json
+
+
+# need to define a data structure for the contacts, like a model in MVC
+
+
+
+# on startup, load the contacts from the file
+
+
+
+# need to utilize CRUD operations on the contacts
+
+
+
+# on exit, save the contacts to the file - or maybe save on every change?
+
+
+
+# search and sort operations?
